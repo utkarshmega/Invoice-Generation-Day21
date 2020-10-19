eaef392cdb2bd9c79c57f23330775d3d8c9c6bb3
utkarshmega <utkarsheduc@gmail.com>
2020-10-19 22:43:30 +0530

    Welcome to cab invoice generation

---
+++ src/com/training/invoicegeneration/CabInvoice.java
@@ -0,0 +1,11 @@
+package com.training.invoicegeneration;
+
+public class CabInvoice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Cab invoice generation");
+
+	}
+
+}
